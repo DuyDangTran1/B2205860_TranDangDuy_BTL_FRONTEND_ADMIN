e3d22dd3eac48693bf8939421bf1f7be09201db4
DangDuyTran1 <tuechst1@gmail.com>
2025-12-02 19:20:56 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from "vue-router";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
+
+<template>
+  <RouterView></RouterView>
+</template>
+
+<style lang="css" scoped></style>
